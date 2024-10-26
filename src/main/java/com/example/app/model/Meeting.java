046acff1d9e2e45ff3d794cc2f9e84334350cb93
Savinu <savinu.hsa@gmail.com>
2024-10-26 17:59:12 +0530

    add new contructors, new attribute date, startTime, endTime

--- src/main/java/com/example/app/model/Meeting.java
+++ src/main/java/com/example/app/model/Meeting.java
@@ -1,24 +1,36 @@
 package com.example.app.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Meeting {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
 
     @ManyToOne
-    @JoinColumn(name = "host_id", nullable = false)  // Foreign key to AppUser entity
+    @JoinColumn(name = "host_id", nullable = false) // Foreign key to AppUser entity
+    @JsonIgnore
     private AppUser host;
+
+    public Meeting(String title, LocalDate date, LocalTime startTime, LocalTime endTime, AppUser host) {
+        this.title = title;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.host = host;
+    }
 }
