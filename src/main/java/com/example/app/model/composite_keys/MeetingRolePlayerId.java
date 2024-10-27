78065e83b6ce6967c83bbd1ab51a046a82c06a59
Savinu <savinu.hsa@gmail.com>
2024-10-27 13:52:15 +0530

    redefine MeetingRolePlayer composite primary key

--- src/main/java/com/example/app/model/composite_keys/MeetingRolePlayerId.java
+++ src/main/java/com/example/app/model/composite_keys/MeetingRolePlayerId.java
@@ -1,9 +1,20 @@
 package com.example.app.model.composite_keys;
 
+import com.example.app.model.types.Role;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 @Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MeetingRolePlayerId {
     private Long meetingId;
-    private String username;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
